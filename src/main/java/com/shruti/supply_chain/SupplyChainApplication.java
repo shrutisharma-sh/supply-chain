08a20d0612b3dc4376fbcdbb70f5a91963b6a79a
shrutisharma-sh <shrutish8439@gmail.com>
2026-03-12 23:13:26 +0530

    initalized supply chain management system project

---
+++ src/main/java/com/shruti/supply_chain/SupplyChainApplication.java
@@ -0,0 +1,13 @@
+package com.shruti.supply_chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shruti/supply_chain/SupplyChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shruti.supply_chain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
