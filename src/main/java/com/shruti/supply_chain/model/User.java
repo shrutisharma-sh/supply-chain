5cae6769c9dbb82936577ea4eb5dd4162535adfe
shrutisharma-sh <shrutish8439@gmail.com>
2026-03-13 23:23:18 +0530

    added roles

---
+++ src/main/java/com/shruti/supply_chain/model/User.java
@@ -0,0 +1,26 @@
+package com.shruti.supply_chain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
